17d9cca44f633703f9ef94a333ef8111c29e3fd7
Thomas Dreibholz <dreibh@simula.no>
2018-10-09 15:52:45 +0200

    Improved script.

--- udpping/run-on-node.sh
+++ udpping/run-on-node.sh
@@ -2,10 +2,11 @@
 
 MONROE_NAMESPACE=$(docker ps --no-trunc -aqf name=monroe-namespace)
 
-DIR=/run/shm/test0
-rm -rf $DIR
-mkdir -p $DIR
-echo '{"nodeid": "999", "measurement_id": "99999", "interface": "op0", "network_id": "99"}' >$DIR/config
-mkdir -p $DIR/results
+WORKDIR=/run/shm/test0
+rm -rf $WORKDIR
+mkdir -p $WORKDIR
+echo '{ "node_id": "999", "instances": [ { "measurement_id": "99999", "interface": "op0", "network_id": "99" }, { "measurement_id": "99998", "interface": "op1", "network_id": "98" } ] }
+' >$WORKDIR/config
+mkdir -p $WORKDIR/results
 
-docker run -d --name test0 --net=container:$MONROE_NAMESPACE   -v $DIR/results:/monroe/results -v $DIR/config:/monroe/config:ro dreibh/udpping
+docker run -d --name test0 --net=container:$MONROE_NAMESPACE   -v $WORKDIR/results:/monroe/results -v $WORKDIR/config:/monroe/config:ro dreibh/udpping
